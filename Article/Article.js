9d5a063e595eaab536e4b504cdd35573a1867542
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-15 13:43:37 +0000

    feature: Add feature to close/hide article when user is done reading

--- Article/Article.js
+++ Article/Article.js
@@ -4,20 +4,32 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    console.log(domElement)
+    console.log(articles.length)
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandArticle = this.expandArticle.bind(this);
     this.expandButton.addEventListener('click', this.expandArticle);
     // this.expandButton.addEventListener('click', () => this.expandArticle());
+
+    // STRETCH GOALS - Close article
+    this.close = document.createElement('span');
+    this.close.textContent = 'Close';
+    this.close.setAttribute('style', 'position: absolute; top: 5px; left: 92%; font-size: 12px;' + 
+      'color: red; cursor: pointer')
+    this.domElement.append(this.close);
+    this.close.addEventListener('click', () => this.closeArticle())
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
+  }
+
+  closeArticle() {
+    this.domElement.style.display = 'none';
   }
 }
 
