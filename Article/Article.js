d28608b1ce1fa6227080e95e2169322f33ef1927
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-15 12:10:07 +0000

    feature: Add an article-open class when an article is clicked

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,20 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+    console.log(domElement)
     // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton');
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.expandArticle = this.expandArticle.bind(this);
     this.expandButton.addEventListener('click', this.expandArticle);
+    // this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle("article-open");
   }
 }
 
